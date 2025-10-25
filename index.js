a308665201f41d8c5362f16a36156e9f6cef2d6a
abbossth <a@test.ru>
2025-10-25 11:00:22 +0500

    tg bot yaratildi

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+import TelegramBot from "node-telegram-bot-api";
+
+const TOKEN = "7692042778:AAE1QmPLBW8T3mCrmU4YpZicSBCLu8z1dvU";
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+bot.on("message", function (msg) {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  console.log(msg);
+
+  bot.sendMessage(chatId, `--> ${text} `);
+});
+
 console.log("Bot ishga tushdi...");
